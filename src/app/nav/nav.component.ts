0233fd4a48eac9d3867764c08770c1d50ca82688
MahiShoor <mahendrashoor40@gmail.com>
2020-05-09 12:48:12 +0530

    End of section 7 Routing

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { AlerifyService } from '../_services/alerify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AlerifyService } from '../_services/alerify.service';
 })
 export class NavComponent implements OnInit {
 model: any = {};
-  constructor(public authService: AuthService, private alertify: AlerifyService) { }
+  constructor(public authService: AuthService, private alertify: AlerifyService, private  router: Router) { }
 
   ngOnInit() {
   }
@@ -20,7 +21,11 @@ model: any = {};
       next => {
       this.alertify.success('logged in successfully');
       }, error => {
-    this.alertify.error(error); }
+    this.alertify.error(error); },
+
+    () => {
+      this.router.navigate(['/members'])
+    }
     );
 
   }
@@ -31,6 +36,8 @@ model: any = {};
 logout() {
   localStorage.removeItem('token');
   this.alertify.message('logged out');
+  this.router.navigate(['/home']);
+
 }
 
 }
