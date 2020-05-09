c2a677bea42e2eb67c439dc4e8a44a344c5c4308
MahiShoor <mahendrashoor40@gmail.com>
2020-05-09 12:47:51 +0530

    End of section 7 Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MembersListComponent } from './members-list/members-list.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { AuthGuard } from './_gaurds/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  {
+     path: '', // 'dummmy' then localhost:4200/dummymemebers
+      runGuardsAndResolvers: 'always',
+canActivate: [AuthGuard],
+children : [
+  { path: 'members', component: MembersListComponent, canActivate: [AuthGuard]},
+  { path: 'messages', component: MessagesComponent},
+  { path: 'list', component: ListsComponent}
+]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full'},
+
+];
 
 @NgModule({
+  declarations: [
+
+    MembersListComponent,
+    ListsComponent,
+    MessagesComponent,
+ 
+  ],
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
